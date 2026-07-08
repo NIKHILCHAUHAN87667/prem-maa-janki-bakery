1bb77b895cc44a4b5d0cbd5701a31c2382bf866d
NIKHILCHAUHAN87667 <nikhilchauhan87667@gmail.com>
2026-07-08 22:22:34 +0530

    Clean up unused image imports in assets.jsx
    Removed unused image imports for various snacks.

--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -9,18 +9,7 @@ import moong_dal_image from "./moong-dal.png";
 
 
 
-import chips_image from "./chips.png";
-import banana_image from "./banana.png";
-import gulab_jamun_image from "./gulab-jamun.png";
-import rasgulla_image from "./rasgulla.png";
-import dairymilk_image from "./dairymilk.png";
-import kitkat_image from "./kitkat.png";
-import coke_image from "./coke.png";
-import pepsi_image from "./pepsi.png";
-import vanilla_ice_image from "./vanilla-ice.png";
-import choco_ice_image from "./choco-ice.png";
-import pavbhaji_image from "./pavbhaji.png";
-import panipuri_image from "./panipuri.png";
+
 
 // Dairy
 import amul_taaza_image from "./amul-taaza.png";
