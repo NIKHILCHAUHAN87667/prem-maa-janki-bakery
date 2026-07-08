7f509b72896bd5a65dc49ccd78ca2d6a6136dfa0
NIKHILCHAUHAN87667 <nikhilchauhan87667@gmail.com>
2026-07-08 22:13:42 +0530

    Refactor image imports in assets.jsx
    Fixed import statement formatting for clarity.

--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -5,7 +5,7 @@ import twist_image from "./twist.jpg";
 import aloo_bhujia_image from "./aloo-bhujia.png";
 import aloo_bhujia_2 from "./aloo-bhujia2.png";
 
-import moong_dal_image from "./moong-dal.png";import bakery_image from "./bakery.jpg";
+import moong_dal_image from "./moong-dal.png";
 import wheat_image from "./wheat.JPG";
 import twist_image from "./twist.jpg";
 import aloo_bhujia_image from "./aloo-bhujia.png";
